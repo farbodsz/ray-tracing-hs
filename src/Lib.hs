16e2c603004bd2ced97ae93649f811f48f3f49a1
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2023-06-04 19:22:30 +0100

    refactor(2.2): make data types clearer

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,30 +2,55 @@
 
 module Lib (main) where
 
-import Control.Monad (forM_)
 import GHC.Float (double2Int, int2Double)
 
 --------------------------------------------------------------------------------
 
 -- Entry point
 main :: IO ()
-main = renderImage testImage
-
-debugImg :: Image -> IO ()
-debugImg img = mapM_ putStrLn output
-  where
-    output = concatMap (map pxlToStr) img
-    pxlToStr (Pixel r g b) = unwords $ show <$> [r, g, b]
+main = mapM_ putStrLn (renderImage testImage)
 
 --------------------------------------------------------------------------------
+-- Types
 
--- Image
+-- | PPM image (portable Pix Map has an image header and body).
+data PPM = PPM
+    { ppmFormat :: String
+    , ppmCols :: Int
+    , ppmRows :: Int
+    , ppmMaxColorVal :: Int
+    , ppmBody :: ImageBody
+    }
+
+type ImageBody = [[Pixel]]
+
+-- | For now, this will be RGB in range [0, 255]
+data Pixel = Pixel
+    { pxlR :: Int
+    , pxlG :: Int
+    , pxlB :: Int
+    }
+    deriving (Eq, Show)
+
+--------------------------------------------------------------------------------
+-- Test image
+
 imgW, imgH :: Int
 imgW = 256
 imgH = 256
 
-testImage :: Image
-testImage = map mkRow ys
+testImage :: PPM
+testImage =
+    PPM
+        { ppmFormat = "P3"
+        , ppmCols = imgW
+        , ppmRows = imgH
+        , ppmMaxColorVal = 255
+        , ppmBody = testImageBody
+        }
+
+testImageBody :: ImageBody
+testImageBody = map mkRow ys
   where
     mkRow j = map (\i -> calcPixel (int2Double i) (int2Double j)) xs
 
@@ -38,28 +63,26 @@ testImage = map mkRow ys
             (j / (int2Double imgH - 1))
             0.25
 
---------------------------------------------------------------------------------
-
--- | RGB in range [0, 255]
-data Pixel = Pixel
-    { pxlR :: Int
-    , pxlG :: Int
-    , pxlB :: Int
-    }
-    deriving (Eq, Show)
-
 mkPixel :: Double -> Double -> Double -> Pixel
 mkPixel r g b = Pixel (to256 r) (to256 g) (to256 b)
   where
     to256 x = double2Int $ x * 255.999
 
-type Image = [[Pixel]]
+--------------------------------------------------------------------------------
+-- Rendering
 
--- Render
-renderImage :: Image -> IO ()
-renderImage img = do
-    putStr $ concat ["P3\n", show imgW, " ", show imgH, "\n255\n"]
-    forM_ img $ \row ->
-        forM_ row $ \p -> putStrLn . unwords $ show <$> [pxlR p, pxlG p, pxlB p]
+renderImage :: PPM -> [String]
+renderImage ppm = renderHeaders ppm ++ renderImageBody (ppmBody ppm)
+  where
+    renderHeaders PPM {..} =
+        [ ppmFormat
+        , show ppmCols ++ " " ++ show ppmRows
+        , show ppmMaxColorVal
+        ]
+
+renderImageBody :: ImageBody -> [String]
+renderImageBody = concatMap (map pxlToStr)
+  where
+    pxlToStr (Pixel r g b) = unwords $ show <$> [r, g, b]
 
 --------------------------------------------------------------------------------
